6ed1c21b5e9503ed9949c87ea80f835e7703b18e
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-22 21:12:00 -0300

    trabajo en la animacion del splash

--- FindMe/app/src/main/java/org/pasut/android/findme/activities/SplashActivity.java
+++ FindMe/app/src/main/java/org/pasut/android/findme/activities/SplashActivity.java
@@ -1,19 +1,26 @@
 package org.pasut.android.findme.activities;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
 
 import org.pasut.android.findme.R;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
 @ContentView(R.layout.activity_splash)
 public class SplashActivity extends RoboActivity {
+    @InjectView(R.id.splash)
+    private ImageView splash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        splash.setBackgroundResource(R.drawable.splash_animation);
+
+        AnimationDrawable anim = (AnimationDrawable) splash.getBackground();
+        anim.start();
     }
 }
