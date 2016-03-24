5906f5650543e52f0c61297d5a776d4e439ac233
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-24 12:23:58 -0300

    implemento la llamada a la pantalla de login

---
+++ FindMe/app/src/main/java/org/pasut/android/findme/activities/LoginActivity.java
@@ -0,0 +1,18 @@
+package org.pasut.android.findme.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.pasut.android.findme.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
--- FindMe/app/src/main/java/org/pasut/android/findme/activities/SplashActivity.java
+++ FindMe/app/src/main/java/org/pasut/android/findme/activities/SplashActivity.java
@@ -1,19 +1,33 @@
 package org.pasut.android.findme.activities;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
+import com.google.inject.Inject;
+
+import org.pasut.android.findme.FindmeApplication;
 import org.pasut.android.findme.R;
+import org.pasut.android.findme.service.PreferencesService;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
 
+import static org.pasut.android.findme.FindmeApplication.MASTER_USER_KEY;
+
 @ContentView(R.layout.activity_splash)
 public class SplashActivity extends RoboActivity {
+    public final static int RESPONSE = 1;
+    private final static String TAG = SplashActivity.class.getSimpleName();
+
     @InjectView(R.id.splash)
     private ImageView splash;
+
+    @Inject
+    private PreferencesService preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +36,14 @@ public class SplashActivity extends RoboActivity {
 
         AnimationDrawable anim = (AnimationDrawable) splash.getBackground();
         anim.start();
+
+        if(!preferences.contain(MASTER_USER_KEY)) {
+            Log.d(TAG, "Todavia no se registro ningun usuario");
+            startActivityForResult(new Intent(this, LoginActivity.class), RESPONSE);
+        } else {
+            Log.d(TAG, "Ya existe un usuario registrado");
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
     }
 }
