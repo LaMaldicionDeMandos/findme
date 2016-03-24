66fd79a1fbbec6b94c5aa8e28f934c5b7f54125a
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-24 17:34:37 -0300

    Integro google sign in

--- FindMe/app/src/main/java/org/pasut/android/findme/activities/LoginActivity.java
+++ FindMe/app/src/main/java/org/pasut/android/findme/activities/LoginActivity.java
@@ -1,18 +1,110 @@
 package org.pasut.android.findme.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.pasut.android.findme.R;
 
-public class LoginActivity extends AppCompatActivity {
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+
+import static org.pasut.android.findme.activities.ActivityUtils.getGoogleClient;
+
+@ContentView(R.layout.activity_login)
+public class LoginActivity extends RoboActivity implements GoogleApiClient.ConnectionCallbacks,
+GoogleApiClient.OnConnectionFailedListener {
+    private final static String TAG = LoginActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 0;
+
+    @InjectView(R.id.sign_in_button)
+    private SignInButton googleButton;
+
+    private GoogleApiClient googleClient;
+
+    private boolean mIsResolving;
+    private boolean mShouldResolve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mShouldResolve = true;
+                googleClient.connect();
+            }
+        });
+        googleClient = getGoogleClient(this, this, this);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // If the error resolution was not successful we should not resolve further.
+            if (resultCode != RESULT_OK) {
+                mShouldResolve = false;
+            }
+
+            mIsResolving = false;
+            setResult(RESULT_OK);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        googleClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "Google account connected");
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "Google account suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "Google account failed " + connectionResult);
+        // Could not connect to Google Play Services.  The user needs to select an account,
+        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
+        // ConnectionResult to see possible error codes.
+
+        if (!mIsResolving && mShouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mIsResolving = true;
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e(TAG, "Could not resolve ConnectionResult.", e);
+                    mIsResolving = false;
+                    googleClient.connect();
+                }
+            } else {
+                // Could not resolve the connection result, show the user an
+                // error dialog.
+                Log.d(TAG, connectionResult.toString());
+            }
+        } else {
+            // Show the signed-out UI
+            Log.d(TAG, "Parece estar todo ok");
+        }
+    }
 }
